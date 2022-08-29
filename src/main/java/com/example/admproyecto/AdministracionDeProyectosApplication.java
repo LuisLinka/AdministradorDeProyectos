83e3b943a40721f2ac691201b82778bf1daa9186
JoseLeviRivera <joselevirivera@gmail.com>
2022-08-29 14:05:17 -0600

    Primer commit

---
+++ src/main/java/com/example/admproyecto/AdministracionDeProyectosApplication.java
@@ -0,0 +1,13 @@
+package com.example.admproyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdministracionDeProyectosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdministracionDeProyectosApplication.class, args);
+	}
+
+}
